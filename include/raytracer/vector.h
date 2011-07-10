334bb9e1902435f98278609dabbdf3128e759e65
Andreas Ots <andreasots@gmail.com>
2011-07-10 13:26:56 +0300

    template based Vector class

--- include/raytracer/vector.h
+++ include/raytracer/vector.h
@@ -3,54 +3,91 @@
 
 namespace Raytracer
 {
-class Mult
-{
-public:
-    Mult(Vector v, Vector u)
-};
+    template<class T = float, size_t N = 3>
+    class Vector
+    {
+        public:
+        Vector(std::initializer_list<T> list)
+        {
+            if(list.size() < N)
+            {
+                size_t i;
+                for(i = 0; i < list.size(); i++)
+                    m_vector[i] = *(list.begin()+i);
+                for(; i < N; i++)
+                    m_vector[i] = 0;
+            }
+            else
+                for(size_t i = 0; i < N; i++)
+                    m_vector[i] = *(list.begin()+i);
+        }
 
-class Vector
-{
-public:
-    Vector(float x, float y, float z);
-    float x()
-    {
-        return m_x;
-    }
-    void x(float val)
-    {
-        m_x = val;
-    }
-    float y()
-    {
-        return m_y;
-    }
-    void y(float val)
-    {
-        m_y = val;
-    }
-    float z()
-    {
-        return m_z;
-    }
-    void z(float val)
-    {
-        m_z = val;
-    }
-    float w()
-    {
-        return m_w;
-    }
-    void w(float val)
-    {
-        m_w = val;
-    }
-protected:
-private:
-    float m_x;
-    float m_y;
-    float m_z;
-    float m_w;
+        Vector(Vector<T, N> &vec)
+        {
+            for(size_t i = 0; i < N; i++)
+                m_vector[i] = vec[i];
+        }
+
+        Vector()
+        {
+            for(size_t i = 0; i < N; i++)
+                m_vector[i] = 0;
+        }
+
+        Vector<T, N+1> homogeneous()
+        {
+            Vector<T, N+1> ret;
+            for(size_t i = 0; i < N; i++)
+                ret[i] = m_vector[i];
+            ret[N] = 1;
+            return ret;
+        }
+
+        Vector<T, N-1> project()
+        {
+            Vector<T, N-1> ret;
+            for(size_t i = 0; i < N-1; i++)
+                ret[i] = m_vector[i]/m_vector[N-1];
+            return ret;
+        }
+
+        Vector<T, N>& add(Vector<T, N> vec)
+        {
+            for(size_t i = 0; i < N; i++)
+                m_vector[i] += vec[i];
+            return *this;
+        }
+
+        Vector<T, N> cross(Vector<T, N> vec)
+        {
+            throw Exception("Vector::cross: Not defined");
+        }
+
+        T dot(Vector<T, N> vec)
+        {
+            T ret;
+            for(size_t i = 0; i < N; i++)
+                ret += m_vector * vec[i];
+            return ret;
+        }
+
+        Vector<T, N>& div(T x)
+        {
+            for(size_t i = 0; i < N; i++)
+                m_vector[i] /= x;
+            return *this;
+        }
+
+        Vector<T, N>& normalise()
+        {
+            T length = this->dot(*this);
+            if(length != 1)
+                this->div(sqrt(length));
+            return *this;
+        }
+
+        protected:
+        T m_vector[N];
 };
 
 } // namespace Raytracer
